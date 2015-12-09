2b1fdf24862b000479c8ecec77822e0710e17432
Daniel Santa Maria <santamaria@utexas.edu>
2015-12-09 16:40:09 -0600

    added workout summary

--- android/app/src/main/java/com/mycompany/workoutbuddy/GenWorkout.java
+++ android/app/src/main/java/com/mycompany/workoutbuddy/GenWorkout.java
@@ -9,9 +9,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import java.util.List;
+import java.util.ArrayList;
+import android.content.Context;
 
 public class GenWorkout extends ActionBarActivity {
     private String TAG  = "GenWorkout";
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +32,27 @@ public class GenWorkout extends ActionBarActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] response) {
                 try {
+                    ArrayAdapter<String> adapter, adapter2;
+                    ListView  listview1 = (ListView) findViewById(R.id.lstLeft);
+                    ListView  listview2 = (ListView) findViewById(R.id.lstRight);
+                    List<String> exList = new ArrayList<String>();
+                    List<String> setsList = new ArrayList<String>();
                     JSONObject jObject = new JSONObject(new String(response));
+                    String category = jObject.getString("category");
+                    String reps = jObject.getString("reps");
+                    //TextView textView = (TextView) findViewById(R.id.category);
+                    //textView.setText("Category: " + category);
                     JSONArray ExerciseList = jObject.getJSONArray("exercises");
                     for(int i=0; i < ExerciseList.length(); i++) {
-                        System.out.println(ExerciseList.get(i));
-                        //JSONObject exercise = ExerciseList.getJSONObject(i);
-                        //System.out.println("exercise is " + exercise);
+                        exList.add((String) ExerciseList.get(i));
+                        setsList.add(reps);
+                        System.out.println(exList.get(i));
                     }
+                    adapter=new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, exList);
+                    listview1.setAdapter(adapter);
+                    adapter2=new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, setsList);
+                    listview2.setAdapter(adapter2);
+
                 }
                 catch (JSONException j){
                     System.out.println("JSON Error");
