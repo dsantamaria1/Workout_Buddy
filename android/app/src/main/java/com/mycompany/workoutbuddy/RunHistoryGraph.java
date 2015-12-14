b2a7f96e17461d88ccf19908273399d978c2303c
Daniel Santa Maria <santamaria@utexas.edu>
2015-12-13 20:10:47 -0600

    adding run history from server

--- android/app/src/main/java/com/mycompany/workoutbuddy/RunHistoryGraph.java
+++ android/app/src/main/java/com/mycompany/workoutbuddy/RunHistoryGraph.java
@@ -43,37 +43,30 @@ public class RunHistoryGraph extends ActionBarActivity {
                     JSONArray keyList = jObject.getJSONArray("testList");
                     JSONArray dateList = jObject.getJSONArray("dateList");
                     JSONObject timeDict = jObject.getJSONObject("timeDict");
+                    DataPoint[] datapt = {new DataPoint(0,0), new DataPoint(0,0),new DataPoint(0,0),
+                                          new DataPoint(0,0), new DataPoint(0,0)};
+                    String[] dates = {"","","","",""};
 
                     for(int i = 0; i < keyList.length(); i++) {
                         System.out.println("starting prints");
                         System.out.println(keyList.getString(i));
                         System.out.println(timeDict.getDouble(keyList.getString(i)));
                         System.out.println(dateList.getString(i));
+                        datapt[i] = new DataPoint(i, timeDict.getDouble(keyList.getString(i)));
+                        dates[i] = dateList.getString(i);
                     }
-
-
-
-
-
 
                     GraphView graph = (GraphView) findViewById(R.id.graph);
 // you can directly pass Date objects to DataPoint-Constructor
 // this will convert the Date to double via Date#getTime()
-                    BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(new DataPoint[] {
-                            new DataPoint(1, 30.3),
-                            new DataPoint(2, 29.5),
-                            new DataPoint(3, 12.3),
-                            new DataPoint(4, 18.6),
-                            new DataPoint(5, 25.7)
-                    });
+                    BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(datapt);
                     graph.addSeries(series);
 
 // set date label formatter
                     //graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(context));
                     //graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
-                    String[] WEEK_DAYS = {"12/12", "12/13", "12/14", "12/15", "12/10"};
                     StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-                    staticLabelsFormatter.setHorizontalLabels(WEEK_DAYS);
+                    staticLabelsFormatter.setHorizontalLabels(dates);
                     graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
                     graph.getGridLabelRenderer().setGridStyle(GridStyle.NONE);
 
